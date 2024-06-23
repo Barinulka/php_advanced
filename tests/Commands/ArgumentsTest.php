fc6b98e26b356ae4512b3cef9c637d3078bf70ad
al.barinov <al.barinov@academcity.online>
2024-06-23 12:53:15 +0300

    add throw exception test

--- tests/Commands/ArgumentsTest.php
+++ tests/Commands/ArgumentsTest.php
@@ -3,6 +3,7 @@
 namespace App\UnitTests\Commands;
 
 use App\Commands\Arguments;
+use App\Exception\ArgumentsException;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentsTest extends TestCase
@@ -23,5 +24,18 @@ class ArgumentsTest extends TestCase
         $this->assertSame('123', $value);
         $this->assertEquals('123', $value);
         $this->assertIsString($value);
+    }
+
+    public function testItThrowsAnExceptionWhenArgumentIsAbsent(): void
+    {
+        // Подготавливаем объект с пустыми данными
+        $command = new Arguments([]);
+
+        // Описываем ожидаемый тип исключения и его сообщения
+        $this->expectException(ArgumentsException::class);
+        $this->expectExceptionMessage('Не передан аргумент: some_key');
+
+        // ВЫполняем действие приводящее к исключению
+        $command->get('some_key');
     }
 }
